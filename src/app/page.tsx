08a4fe993ea53948893e40c5f8ce6e26743d9dd5
Omkar Date <omkardate29@gmail.com>
2024-12-15 02:48:43 +0530

    fix(page): set dynamic rendering to force-dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { TodoList } from "@/components/todo-list";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return (
     <main className="container mx-auto p-4 max-w-lg">
